18ed9297fec98b1b94df275623bfebb23535bd62
Polina Izvekova <izvekova@gmail.com>
2025-01-06 13:46:44 +0100

    window.innerHeight

--- vendor/pycs-layout.jquery.js
+++ vendor/pycs-layout.jquery.js
@@ -103,9 +103,9 @@
        */
       var get_aspect_ratios = function (items) {
         // Calculate ideal height based on screen orientation
-        var idealHeight = Math.round(window.outerHeight > window.outerWidth ? 
-                                     (window.outerHeight / 2) : 
-                                     (window.outerHeight / 2.5));
+        var idealHeight = Math.round(window.innerHeight > window.innerWidth ? // Compares the height and width of the window.
+                                     (window.innerHeight / 2) :   // The window is taller than it is wide (portrait mode).
+                                     (window.innerHeight / 2.5)); // The window is wider than it is tall (landscape mode).
         var aspect_ratios = [];
 
         for (var i = 0; i < items.length; i++) {
